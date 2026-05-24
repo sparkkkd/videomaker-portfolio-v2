7ace63f8bed81b51ce2917ccf8284584424f9358
d.akaev <d.akaev>
2026-05-24 19:32:19 +0300

    comment middleware for test

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(request: NextRequest) {
-	const { pathname } = request.nextUrl
+// export function middleware(request: NextRequest) {
+// 	const { pathname } = request.nextUrl
 
-	if (pathname.startsWith('/admin') || pathname.startsWith('/login')) {
-		const refreshToken = request.cookies.get('refreshToken')?.value
+// 	if (pathname.startsWith('/admin') || pathname.startsWith('/login')) {
+// 		const refreshToken = request.cookies.get('refreshToken')?.value
 
-		if (refreshToken && pathname.startsWith('/login')) {
-			const adminUrl = new URL('/admin', request.url)
-			return NextResponse.redirect(adminUrl)
-		}
+// 		if (refreshToken && pathname.startsWith('/login')) {
+// 			const adminUrl = new URL('/admin', request.url)
+// 			return NextResponse.redirect(adminUrl)
+// 		}
 
-		if (!refreshToken && pathname.startsWith('/admin')) {
-			const loginUrl = new URL('/login', request.url)
-			return NextResponse.redirect(loginUrl)
-		}
-	}
+// 		if (!refreshToken && pathname.startsWith('/admin')) {
+// 			const loginUrl = new URL('/login', request.url)
+// 			return NextResponse.redirect(loginUrl)
+// 		}
+// 	}
 
-	return NextResponse.next()
-}
+// 	return NextResponse.next()
+// }
 
-export const config = {
-	matcher: ['/admin/:path*', '/login/:path*'],
-}
+// export const config = {
+// 	matcher: ['/admin/:path*', '/login/:path*'],
+// }
