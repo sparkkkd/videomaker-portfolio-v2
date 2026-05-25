8686062decf47558186e5a8df60df1524437841c
d.akaev <d.akaev>
2026-05-26 00:12:43 +0300

    add standalone build

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
+	output: 'standalone',
+
 	images: {
 		dangerouslyAllowLocalIP: process.env.NODE_ENV === 'development',
 
